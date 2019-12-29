12dd45893014fdf33dc78d11d82601a10033b0e5
Truong <tientruong@coloralpha.com>
2019-12-29 22:08:59 +0700

    code full deivce(imcomplete)

--- app/src/main/java/com/iot/smarthome/AppConfig.java
+++ app/src/main/java/com/iot/smarthome/AppConfig.java
@@ -1,7 +1,7 @@
 package com.iot.smarthome;
 
 public class AppConfig {
-    public final static String URL_SERVER = "https://smarthome116.herokuapp.com";
+    public final static String URL_SERVER = "https://smart-home-hung.herokuapp.com";
     //public final static String URL_SERVER = "http://192.168.0.103:3484";
     public final static String NAMESPACE_GET_DATA = "/android";
 
