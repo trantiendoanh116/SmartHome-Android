74b4d4d74fd882227a84cc9e800632566346bba2
TienTruong <tientruong@coloralpha.com>
2019-12-30 17:19:53 +0700

    full device

--- app/src/main/java/com/iot/smarthome/AppConfig.java
+++ app/src/main/java/com/iot/smarthome/AppConfig.java
@@ -7,4 +7,29 @@ public class AppConfig {
 
     public final static String EVENT_RECEIVE_DATA = "DATA";
     public final static String EVENT_CONTROL = "CONTROL";
+
+    public final static String den_tran_kh1 = "F1_D01";
+    public final static String den_chum_kh1 = "F1_D01";
+    public final static String den_tranh_kh1 = "F1_D01";
+    public final static String quat_tran = "F1_D01";
+    public final static String den_trangtri_kh1 = "F1_D01";
+    public final static String den_tran_kh2 = "F1_D01";
+    public final static String den_chum_kh2 = "F1_D01";
+    public final static String den_tranh_kh2 = "F1_D01";
+    public final static String den_san = "F1_D01";
+    public final static String den_cong = "F1_D01";
+    public final static String den_wc = "F1_D01";
+    public final static String binh_nl = "F1_D01";
+    public final static String den_cua_ngach = "F1_D01";
+    public final static String den_bep_1 = "F1_D01";
+    public final static String den_bep_2 = "F1_D01";
+    public final static String khi_loc = "F1_D01";
+    public final static String at_bep = "F1_D01";
+    public final static String at_tong = "F1_D01";
+    public final static String temp_humi = "F1_D01";
+    public final static String co = "F1_D01";
+    public final static String dong_dien_tong = "F1_D01";
+    public final static String cong_suat_tieu_thu = "F1_D01";
+
+
 }
--- app/src/main/java/com/iot/smarthome/fragments/HomeFragment.java
+++ app/src/main/java/com/iot/smarthome/fragments/HomeFragment.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -21,10 +22,15 @@ import com.iot.smarthome.activities.HomeActivity;
 
 import org.json.JSONObject;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements View.OnClickListener {
     private final String TAG = HomeFragment.class.getSimpleName();
     private Toolbar mToolbar;
     private TextView mTextWaring;
+    private TextView mTxtDenTranKh1, mTxtDenChumKh1, mTxtDentranhKh1, mTxtQuatTran, mTxtDenTrangTriKh1, mTxtDenTranKh2, mTxtDenChumKh2, mTxtDentranhKh2, mTxtDenSan,
+            mTxtDenCong, mTxtDenWC, mTxtBinhNL, mTxtDenCuaNgach, mTxtDenbep1, mTxtDenBep2, mTxtKhiLoc, mTxtATtong, mTxtATbep, mTxtTemp, mTxtHumi, mTxtCo, mTxtDongDienTong,
+            mTxtCSTieuThu;
+    private Button btnDenTranKh1, btnDenChumKh1, btnDenTranhKh1, btnOffQuatTran, btnOnQuatTran, btnDenTrangTriKh1, btnDenTranKh2, btnDenChumKh2, btnDenTranhKh2, btnDenSan,
+            btnDenCong, btnDenWC, btnBinhNL, btnDenCuaNgach, btnDenBep1, btnDenBep2, bntKhiLoc, btnATtong, btnATbep;
 
     private Socket mSocket;
 
@@ -49,10 +55,74 @@ public class HomeFragment extends Fragment {
 
     private void initViews() {
         mToolbar = getView().findViewById(R.id.toolbar);
+        mTextWaring = getView().findViewById(R.id.text_warning);
+
+        mTxtDenTranKh1 = getView().findViewById(R.id.f1d01_value);
+        btnDenTranKh1 = getView().findViewById(R.id.f1d01_btn);
+
+        mTxtDenChumKh1 = getView().findViewById(R.id.f1d02_value);
+        btnDenChumKh1 = getView().findViewById(R.id.f1d02_btn);
+
+        mTxtDentranhKh1 = getView().findViewById(R.id.f1d03_value);
+        btnDenTranhKh1 = getView().findViewById(R.id.f1d03_btn);
+
+        mTxtQuatTran = getView().findViewById(R.id.f1d04_value);
+        btnOffQuatTran = getView().findViewById(R.id.f1d04_btn_off);
+        btnOnQuatTran = getView().findViewById(R.id.f1d04_btn_on);
+
+        mTxtDenTrangTriKh1 = getView().findViewById(R.id.f1d05_value);
+        btnDenTrangTriKh1 = getView().findViewById(R.id.f1d05_btn);
+
+        mTxtDenTranKh2 = getView().findViewById(R.id.f1d06_value);
+        btnDenTranKh2 = getView().findViewById(R.id.f1d06_btn);
+
+        mTxtDenChumKh2 = getView().findViewById(R.id.f1d07_value);
+        btnDenChumKh2 = getView().findViewById(R.id.f1d07_btn);
+
+        mTxtDentranhKh2 = getView().findViewById(R.id.f1d08_value);
+        btnDenTranhKh2 = getView().findViewById(R.id.f1d08_btn);
+
+        mTxtDenSan = getView().findViewById(R.id.f1d09_value);
+        btnDenSan = getView().findViewById(R.id.f1d09_btn);
+
+        mTxtDenCong = getView().findViewById(R.id.f1d10_value);
+        btnDenCong = getView().findViewById(R.id.f1d10_btn);
+
+        mTxtDenWC = getView().findViewById(R.id.f1d11_value);
+        btnDenWC = getView().findViewById(R.id.f1d11_btn);
+
+        mTxtBinhNL = getView().findViewById(R.id.f1d12_value);
+        btnBinhNL = getView().findViewById(R.id.f1d12_btn);
+
+        mTxtDenCuaNgach = getView().findViewById(R.id.f1d13_value);
+        btnDenCuaNgach = getView().findViewById(R.id.f1d13_btn);
+
+        mTxtDenbep1 = getView().findViewById(R.id.f1d14_value);
+        btnDenBep1 = getView().findViewById(R.id.f1d14_btn);
+
+        mTxtDenBep2 = getView().findViewById(R.id.f1d15_value);
+        btnDenBep2 = getView().findViewById(R.id.f1d15_btn);
+
+        mTxtKhiLoc = getView().findViewById(R.id.f1d16_value);
+        bntKhiLoc = getView().findViewById(R.id.f1d16_btn_on);
+
+        mTxtATtong = getView().findViewById(R.id.cd01_value);
+        btnATtong = getView().findViewById(R.id.cd01_btn);
+
+        mTxtATbep = getView().findViewById(R.id.cd02_value);
+        btnATbep = getView().findViewById(R.id.cd02_btn);
+
+        mTxtTemp = getView().findViewById(R.id.cs01_temp_value);
+        mTxtHumi = getView().findViewById(R.id.cs01_humi_value);
+        mTxtCo = getView().findViewById(R.id.cs02_value);
+        mTxtDongDienTong = getView().findViewById(R.id.cs03_dd_value);
+        mTxtCSTieuThu = getView().findViewById(R.id.cs03_cs_value);
+
+
     }
 
     private void setOnListener() {
-        mTextWaring = getView().findViewById(R.id.text_warning);
+
         mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -65,6 +135,26 @@ public class HomeFragment extends Fragment {
                 }, 350);
             }
         });
+        btnDenTranKh1.setOnClickListener(this);
+        btnDenChumKh1.setOnClickListener(this);
+        btnDenTranhKh1.setOnClickListener(this);
+        btnOffQuatTran.setOnClickListener(this);
+        btnOnQuatTran.setOnClickListener(this);
+        btnDenTrangTriKh1.setOnClickListener(this);
+        btnDenTranKh2.setOnClickListener(this);
+        btnDenChumKh2.setOnClickListener(this);
+        btnDenTranhKh2.setOnClickListener(this);
+        btnDenSan.setOnClickListener(this);
+        btnDenCong.setOnClickListener(this);
+        btnDenWC.setOnClickListener(this);
+        btnBinhNL.setOnClickListener(this);
+        btnDenCuaNgach.setOnClickListener(this);
+        btnDenBep1.setOnClickListener(this);
+        btnDenBep2.setOnClickListener(this);
+        bntKhiLoc.setOnClickListener(this);
+        btnATtong.setOnClickListener(this);
+        btnATbep.setOnClickListener(this);
+
     }
 
     private void setOnListenerSocket() {
@@ -113,6 +203,72 @@ public class HomeFragment extends Fragment {
 
         });
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.f1d01_btn:
+                Log.d(TAG, "Button DenTranKH1 clicked");
+                break;
+            case R.id.f1d02_btn:
+                Log.d(TAG, "Button DenChumKH1 clicked");
+                break;
+            case R.id.f1d03_btn:
+                Log.d(TAG, "Button DenTranhKH1 clicked");
+                break;
+            case R.id.f1d04_btn_off:
+                Log.d(TAG, "Button OFF QuatTran clicked");
+                break;
+            case R.id.f1d04_btn_on:
+                Log.d(TAG, "Button ON QuatTran clicked");
+                break;
+            case R.id.f1d05_btn:
+                Log.d(TAG, "Button DenTrangtriKH1 clicked");
+                break;
+            case R.id.f1d06_btn:
+                Log.d(TAG, "Button DenTranKh2 clicked");
+                break;
+            case R.id.f1d07_btn:
+                Log.d(TAG, "Button DenChumKH2 clicked");
+                break;
+            case R.id.f1d08_btn:
+                Log.d(TAG, "Button DenTranhKH2 clicked");
+                break;
+            case R.id.f1d09_btn:
+                Log.d(TAG, "Button DenSan clicked");
+                break;
+            case R.id.f1d10_btn:
+                Log.d(TAG, "Button DenCong clicked");
+                break;
+            case R.id.f1d11_btn:
+                Log.d(TAG, "Button DenWC clicked");
+                break;
+            case R.id.f1d12_btn:
+                Log.d(TAG, "Button BinhNL clicked");
+                break;
+            case R.id.f1d13_btn:
+                Log.d(TAG, "Button DenCuaNgach clicked");
+                break;
+            case R.id.f1d14_btn:
+                Log.d(TAG, "Button DenBep1 clicked");
+                break;
+            case R.id.f1d15_btn:
+                Log.d(TAG, "Button DenBep2 clicked");
+                break;
+            case R.id.f1d16_btn_on:
+                Log.d(TAG, "Button KhiLoc clicked");
+                break;
+            case R.id.cd01_btn:
+                Log.d(TAG, "Button ATbep clicked");
+                break;
+            case R.id.cd02_btn:
+                Log.d(TAG, "Button ATbep clicked");
+                break;
+            default:
+                break;
+        }
     }
 
     private Emitter.Listener onNewData = new Emitter.Listener() {
